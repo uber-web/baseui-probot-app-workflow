b00e8d8365b48eec3892f4e7593ae2d605fffe2e
Gergely Nemeth <gnemeth@uber.com>
2019-10-15 12:55:04 -0700

    fix(deploy): pin to now version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-yarn global add now
+yarn global add now@11.0.6
 APP_URL=$(now -t $NOW_TOKEN --public -e APP_ID=$APP_ID -e PRIVATE_KEY=$PRIVATE_KEY -e BUILDKITE_TOKEN=$BUILDKITE_TOKEN -e WEBHOOK_SECRET=$WEBHOOK_SECRET -e NODE_ENV="production" -e PUBLISH_ARGS="dist")
 now scale $APP_URL sfo 1 --token=$NOW_TOKEN
 now alias set $APP_URL baseui-probot-app-workflow -t $NOW_TOKEN
