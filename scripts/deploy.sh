515016e31f417a4f676bd9b00b308d8e8d44ae00
Gergely Nemeth <gnemeth@uber.com>
2019-10-15 13:02:06 -0700

    fix(deploy): add --npm flag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 yarn global add now@11.0.6
-APP_URL=$(now -t $NOW_TOKEN --public -e APP_ID=$APP_ID -e PRIVATE_KEY=$PRIVATE_KEY -e BUILDKITE_TOKEN=$BUILDKITE_TOKEN -e WEBHOOK_SECRET=$WEBHOOK_SECRET -e NODE_ENV="production" -e PUBLISH_ARGS="dist")
+APP_URL=$(now --npm -t $NOW_TOKEN --public -e APP_ID=$APP_ID -e PRIVATE_KEY=$PRIVATE_KEY -e BUILDKITE_TOKEN=$BUILDKITE_TOKEN -e WEBHOOK_SECRET=$WEBHOOK_SECRET -e NODE_ENV="production" -e PUBLISH_ARGS="dist")
 now scale $APP_URL sfo 1 --token=$NOW_TOKEN
 now alias set $APP_URL baseui-probot-app-workflow -t $NOW_TOKEN
 now rm baseui-probot-app-workflow --safe -t $NOW_TOKEN -y
